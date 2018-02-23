4170610ce9bbbeef398fde997355ead82680d2f6
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-23 16:57:39 +0700

    Release 5

--- app.js
+++ app.js
@@ -2,4 +2,10 @@
 
 const express = require('express')
 let app = express()
+app.set('view engine', 'ejs')
+const index = require('./routes/index.js')
+const user = require('./routes/user.js')
+app.use('/', index)
+app.use('/user', user)
+
 app.listen(3000)
---
+++ routes/user.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var express = require('express')
+var router = express.Router()
+
+router.get('/', function (req, res) {
+    res.render('user');
+});
+
+router.post('/', (req, res) => {
+    //method here
+    res.redirect('/')
+})
+
+router.get('/edit/:id', (req, res) => {
+    res.render('user-detail')
+})
+
+router.post('/edit/:id', (req, res) => {
+    //method here
+    res.redirect('/')
+})
+
+router.get('/edit/:id', (req, res) => {
+    // res.render('user-detail')
+})
+
+module.exports = router
