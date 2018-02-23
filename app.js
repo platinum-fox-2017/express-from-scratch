ce5d7337304f7884f3f25be9b7bbf807e33a4b5e
Fachrizal Gita <Cecha@Cechas-MacBook-Pro.local>
2018-02-23 17:26:47 +0700

    hehe

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const Sequelize = require('sequelize');
-const bodyParser = require('body-parser');
+const models = require('./models');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
 const index = require('./routes/index.js');
 const user = require('./routes/user.js');
 app.use('/', index);
-app.use('/user', user)
+app.use('/user', user);
 
-app.listen(3000, console.log('AYE AYE CAPTAIN!'))
+app.listen(3000, console.log('AYE AYE CAPTAIN!'));
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render()
+  models.User.findAll().then(dataUser => {
+    res.render('user', {title: 'User List', dataUser: dataUser})
+  }).catch(err => {
+    res.send(err)
+  })
 })
 
 module.exports = router;
