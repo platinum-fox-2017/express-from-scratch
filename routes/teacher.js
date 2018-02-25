c45c83b29ea0e50b1094fd8e11488481065f37ae
Gustaf Pahlevi <gustafpahlevi@gmail.com>
2018-02-26 06:57:42 +0700

    update

--- routes/teacher.js
+++ routes/teacher.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', (req,res) => {
     Teacher.findAll({
-        attributes: ['Subject.subject_name','first_name', 'last_name'],
+        attributes: ['Subject.subject_name','first_name', 'last_name', 'email','id'],
         include: Subject
     })
     .then(data => {
@@ -33,15 +33,24 @@ router.post('/add', (req,res) => {
     }
     Teacher.create(teacherAddObj)
     .then(createSuccess => {
-         Teacher.findAll()
-        .then(data => {
-            Subject.findAll()
-            .then(getSubject => {
-                res.render('teacher', { dataTeacher: data, dataSubject: getSubject })
-            })
-            .catch(err => {
-                console.log(err)
-            })
+        res.redirect('/teacher')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+router.get('/edit/:id', (req,res) => {
+    Teacher.findAll({
+        attributes: ['Subject.subject_name', 'first_name', 'last_name', 'email', 'id'],
+        include: Subject,
+        where: {id:req.params.id}
+    })
+    .then(data => {
+        Subject.findAll()
+        .then(getSubject => {
+            let convertData = JSON.parse(JSON.stringify(data));
+            res.render('teacher-edit', { dataTeacher: convertData[0], dataSubject: getSubject })
         })
         .catch(err => {
             console.log(err)
@@ -50,6 +59,36 @@ router.post('/add', (req,res) => {
     .catch(err => {
         console.log(err)
     })
-})
+});
+
+router.post('/edit/:id', (req,res) => {
+    let teacherEditObj = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        id_subject: req.body.id_subject
+    }
+    Teacher.update(teacherEditObj, {
+        where: {id: req.params.id}
+    })
+    .then(data => {
+         res.redirect('/teacher')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+});
+
+router.get('/delete/:id', (req,res) => {
+    Teacher.destroy({
+        where:{id: req.params.id}
+    })
+    .then(data => {
+         res.redirect('/teacher')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+});
 
 module.exports = router
