bc59701e8c40cc9bff9cc2c5038183cc1018c582
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-27 06:58:39 +0700

    enrolled

--- routes/subjects.js
+++ routes/subjects.js
@@ -4,20 +4,29 @@ const models = require('../models');
 const sequelize = require('sequelize');
 
 routes.get('/', function(request,response){
-  models.Subject.findAll({include:[{model:models.Teacher}],order:sequelize.literal('id ASC')}).then((dataSubject)=>{
-    let obj = {
-      title: 'SUBJECTS',
-      subjects: dataSubject
-    }
-    response.render('subjects/subjects.ejs',obj);
-    // response.send(dataSubject);
+  models.Subject.findAll({include:[{model:models.Teacher}],
+    order:sequelize.literal('id ASC')}).then((dataSubject)=>{
+      let obj = {
+        title: 'SUBJECTS',
+        subjects: dataSubject
+      }
+      response.render('subjects/subjects.ejs',obj);
+      // response.send(dataSubject);
   });
 });
 
-// routes.get('/enrolled/:id', function(request,response){
-//   models.Subject.findById(request.param('id')).then(dataSubject => {
-//     models.Student.findAll()
-//   })
-// })
+routes.get('/enrolled/:id', function(request,response){
+  models.Subject.findAll({include:[{model:models.Student}],
+    where:{id:request.param('id')}
+  }).then((dataSubject)=> {
+    let obj = {
+      title: 'ENROLLED STUDENT',
+      formAction: '/subjects/enrolled',
+      subjects: dataSubject
+    }
+    response.render('subjects/enrolled.ejs',obj)
+    // response.send(dataSubject)
+  })
+})
 
 module.exports = routes;
