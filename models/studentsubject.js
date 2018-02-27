f74e15222e240c43ef7553c3a8650a0fb6c41967
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-27 13:27:02 +0700

    Instance method to get alphabetScore in student's subject

--- models/studentsubject.js
+++ models/studentsubject.js
@@ -11,5 +11,22 @@ module.exports = (sequelize, DataTypes) => {
     StudentSubject.belongsTo(models.Student);
     StudentSubject.belongsTo(models.Subject);
   };
+  StudentSubject.prototype.alphabetScore = function () {
+
+    if (this.score > 90) {
+      return 'A';
+    } else if (this.score <= 90 && this.score > 80) {
+
+      return 'B';
+    } else if (this.score <= 80 && this.score > 70) {
+
+      return 'C';
+    } else if (this.score <= 70 && this.score > 50) {
+
+      return 'D';
+    } else if (this.score <= 50 ) {
+      return 'E';
+    }
+  };
   return StudentSubject;
 };
